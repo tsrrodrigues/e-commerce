46739c354bbaf10ad3b7c6ba12332bbcdab1db72
Tiago <tsrrodrigues@outlook.com>
2020-04-04 01:54:32 -0300

    Creating Put/Patch Routes

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -23,14 +23,26 @@ exports.getAll = async (req, res, next) => {
             value: err
         })
     }
-};
+}
+
+exports.getOne = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id, '_id name email cpf active');
+        return res.send({ user });
+    } catch (err) {
+        return res.status(400).send({
+            error: "List One User failed",
+            value: err
+        })
+    }
+}
+
 
 exports.post = async (req, res, next) => {
     const { errors } = validationResult(req);
     if(errors.length > 0) {
         return res.status(400).send({message: errors})
     }
-
 
     const { email, cpf } = req.body;
     try {
@@ -66,19 +78,6 @@ exports.post = async (req, res, next) => {
             value: err
         })
     }
-};
-
-exports.delete = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const user = await User.findByIdAndDelete(id);
-        res.send({ name: user.name });
-    } catch (err) {
-        return res.status(400).send({
-            error: "Delete failed",
-            value: err
-        })
-    }
 }
 
 exports.auth = async (req, res, next) => {
@@ -105,6 +104,63 @@ exports.auth = async (req, res, next) => {
     });
 }
 
-exports.teste = async (req, res, next) => {
-    res.send({ ok: true, user: req.userId });
+
+exports.put = async (req, res, next) => {
+    try {
+        const { errors } = validationResult(req);
+        if(errors.length > 0) {
+            return res.status(400).send({message: errors})
+        }
+
+        const hash = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hash;
+        const id = req.params.id;
+        const user = await User.findByIdAndUpdate(id, {
+            $set: {
+                name: req.body.name,
+                email: req.body.email,
+                adress: req.body.adress,
+                password: req.body.password,
+            }
+        });
+
+        return res.send({
+            id: user.id, 
+            message: "Usuário editado com sucesso",
+            token: req.headers.authorization
+        });
+    } catch (err) {
+        return res.status(400).send({
+            error: "Edit failed",
+            value: err
+        })
+    }
+}
+
+exports.patch = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const active = req.query.active;
+        const user = await User.findByIdAndUpdate(id, {active: active});
+        return res.send({ message: "Active atualizado com sucesso" });
+    } catch (err) {
+        return res.status(400).send({
+            error: "Edit failed",
+            value: err
+        })
+    }
+}
+
+
+exports.delete = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findByIdAndDelete(id);
+        return res.send({ name: user.name });
+    } catch (err) {
+        return res.status(400).send({
+            error: "Delete failed",
+            value: err
+        })
+    }
 }
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,17 +1,20 @@
-'use strict'
-
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-const registerUserMiddleware = require('../middlewares/registerUser')
+// Importando Middlewares
+const registerUserMiddleware = require('../middlewares/registerUser');
 const authMiddleware = require('../middlewares/auth');
 
 router.get('/', userController.getAll);
+router.get('/:id', userController.getOne);
+
 router.post('/', registerUserMiddleware, userController.post);
-router.delete('/:id', userController.delete);
 router.post('/authenticate', userController.auth);
 
-router.post('/authenticate/teste', authMiddleware, userController.teste);
+router.put('/:id', [authMiddleware, registerUserMiddleware], userController.put);
+router.patch('/:id?active', authMiddleware, userController.patch);
+
+router.delete('/:id', userController.delete);
 
 module.exports = router;
