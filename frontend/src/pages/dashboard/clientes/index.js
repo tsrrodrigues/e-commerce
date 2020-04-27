ef2719815d11a59b4f8ff24b08546e69c5b965c6
Italo Alves <italooko@gmail.com>
2020-04-27 11:44:21 -0300

    Clients page showing users by level, CSS updates

--- frontend/src/pages/dashboard/clientes/index.js
+++ frontend/src/pages/dashboard/clientes/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../../services/api';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -10,7 +11,23 @@ import HeaderTop from '../content/HeaderTop';
 import ModalAddProduto from '../content/ModalAddProduto';
 import ModalAddAviso from '../content/ModalAddAviso';
 
-export default function DashClients() {
+export default function DashClients () {
+    const token = localStorage.getItem('userToken')
+
+    const [clients, setClients] = useState([])
+    const [level, setLevel] = useState(1)
+
+    useEffect(() => {
+        api.get('user', {
+            headers: {
+                Authorization: token,
+            }
+        }).then(response => {
+            setClients(response.data.filter(client => client.access_level === level));
+        })
+
+    }, [token, level]);
+
     document.title = "Clientes";
     
     return (
@@ -31,6 +48,16 @@ export default function DashClients() {
                                         <div className="card-header">
                                             <h2>Clientes</h2>
 
+                                            <div className="btn-group">
+                                                <button className="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <span>Mostrar: </span> Nível {level}
+                                                </button>
+                                                <div className="dropdown-menu">
+                                                    <Link to="#" onClick={() => setLevel(1)}>Nível 1</Link>
+                                                    <Link to="#" onClick={() => setLevel(2)}>Nível 2</Link>
+                                                    <Link to="#" onClick={() => setLevel(3)}>Nível 3</Link>
+                                                </div>
+                                            </div>
                                         </div>
 
                                         <div className="card-body">
@@ -45,54 +72,20 @@ export default function DashClients() {
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    <tr>
-                                                        <th scope="row">Timóteo</th>
-                                                        <td className="hidden-xs hidden-sm">timoteo.lala@email.com</td>
-                                                        <td>(61) 9999-9999</td>
-                                                        <td className="hidden-xs">DF</td>
-                                                        <td>
-                                                            <Link to="/clientes/1" className="btn btn-danger">
-                                                                <i id="icon" className="fa fa-search"></i>
-                                                                <span id="details">Detalhes</span>
-                                                            </Link>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">Henrique</th>
-                                                        <td className="hidden-xs hidden-sm">herique.estilos@email.com</td>
-                                                        <td>(11) 5555-5555</td>
-                                                        <td className="hidden-xs">SP</td>
-                                                        <td>
-                                                            <Link to="/clientes/1" className="btn btn-danger">
-                                                                <i id="icon" className="fa fa-search"></i>
-                                                                <span id="details">Detalhes</span>
-                                                            </Link>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">Alana</th>
-                                                        <td className="hidden-xs hidden-sm">alana.doreino@email.com</td>
-                                                        <td>(51) 3330-3330</td>
-                                                        <td className="hidden-xs">RS</td>
-                                                        <td>
-                                                            <Link to="/clientes/1" className="btn btn-danger">
-                                                                <i id="icon" className="fa fa-search"></i>
-                                                                <span id="details">Detalhes</span>
-                                                            </Link>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row">Catarina</th>
-                                                        <td className="hidden-xs hidden-sm">catarina.pereira@email.com</td>
-                                                        <td>(21) 1111-1111</td>
-                                                        <td className="hidden-xs">RJ</td>
-                                                        <td>
-                                                            <Link to="/clientes/1" className="btn btn-danger">
-                                                                <i id="icon" className="fa fa-search"></i>
-                                                                <span id="details">Detalhes</span>
-                                                            </Link>
-                                                        </td>
-                                                    </tr>
+                                                    {clients.map(client => (
+                                                        <tr key={client._id}>
+                                                            <th scope="row">{client.name}</th>
+                                                            <td className="hidden-xs hidden-sm">{client.email}</td>
+                                                            <td>(61) 9999-9999</td>
+                                                            <td className="hidden-xs">{client.adress.uf}</td>
+                                                            <td>
+                                                                <Link to={`/clientes/${client._id}`} className="btn btn-danger">
+                                                                    <i id="icon" className="fa fa-search"></i>
+                                                                    <span id="details">Detalhes</span>
+                                                                </Link>
+                                                            </td>
+                                                        </tr>
+                                                    ))}
                                                 </tbody>
                                             </table>
                                         </div>
