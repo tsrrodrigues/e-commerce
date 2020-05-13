51ad89a107bcc6f8a6ac36a0268256ccb7564023
Italo Alves <italooko@gmail.com>
2020-05-12 22:41:29 -0300

    'Ajustes' page working with market API

--- frontend/src/pages/dashboard/ajustes/index.js
+++ frontend/src/pages/dashboard/ajustes/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../../services/api';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -10,7 +11,86 @@ import ModalAddProduto from '../content/ModalAddProduto';
 import ModalAddAviso from '../content/ModalAddAviso';
 
 export default function DashConfig () {
+
     const user_name = localStorage.getItem('userDisplayName')
+    const token = localStorage.getItem('userToken')
+
+    const [market, setMarket] = useState({ adress: {} })
+
+    useEffect(() => {
+        api.get('market', {
+            headers: {
+                Authorization: token,
+            },
+            errorHandler: true,
+
+        }).then(response => {
+            setMarket(response.data)
+        })
+
+    }, [token])
+
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [email, setEmail] = useState('')
+    const [cnpj, setCNPJ] = useState('')
+
+    const [cep, setCep] = useState('')
+    const [logradouro, setLogradouro] = useState('')
+    const [complemento, setComplemento] = useState('')
+    const [bairro, setBairro] = useState('')
+    const [localidade, setLocalidade] = useState('')
+    const [uf, setUf] = useState('')
+
+    async function handleMarketEdit (e) {
+        e.preventDefault();
+        
+        if (!market.adress && (!cep || !logradouro || !complemento || !bairro || !localidade || !uf)) {
+            alert("Preencha todos os dados de endereço");
+            return
+        }
+
+        const data = {
+            name: name? name :  market.name,
+            description: description? description : market.description,
+            email: email? email : market.email,
+            cnpj: cnpj? cnpj : market.cnpj,
+
+            adress : {
+                cep: cep? cep: market.adress.cep,
+                logradouro: logradouro? logradouro : market.adress.logradouro,
+                complemento: complemento? complemento : market.adress.complemento,
+                bairro: bairro? bairro : market.adress.bairro,
+                localidade: localidade? localidade : market.adress.localidade,
+                uf: uf? uf : market.adress.uf,
+            }
+        };
+
+        if (market.adress) {
+            await api.put(`market`, data, {
+                headers: {
+                    Authorization: token,
+                },
+                successHandler: true,
+                errorHandler: true,
+
+            }).then(response => {
+                setMarket(response.data)
+            })
+        }
+        else {
+            await api.post(`market`, data, {
+                headers: {
+                    Authorization: token,
+                },
+                successHandler: true,
+                errorHandler: true,
+
+            }).then(response => {
+                setMarket(response.data)
+            })
+        }
+    }
 
     document.title = "Ajustes";
 
@@ -25,6 +105,8 @@ export default function DashConfig () {
                         
                         <div className="user-dashboard">
                             <h1>Olá, {user_name}</h1>
+                            <div id="info-div"></div>
+
                             <div className="row">
                                 <div className="col-md-10 col-sm-11 col-xs-12">
 
@@ -34,24 +116,155 @@ export default function DashConfig () {
                                         </div>
 
                                         <div className="card-body">
-                                            <form>
+                                            <form onSubmit={handleMarketEdit}>
                                                 <div className="form-group">
                                                     <label>Título do site</label>
-                                                    <input type="text" className="form-control" placeholder="E-Commerce"/>
+                                                    <input
+                                                        defaultValue={market.name}
+                                                        type="text"
+                                                        className="form-control"
+                                                        onChange={e => setName(e.target.value)}
+                                                        name="site"
+                                                        required
+                                                    />
                                                 </div>
                                                 <div className="form-group">
                                                     <label>Descrição</label>
-                                                    <input type="text" className="form-control" placeholder="Descreva em poucas palavras"/>
+                                                    <textarea
+                                                        defaultValue={market.description}
+                                                        className="form-control"
+                                                        onChange={e  => setDescription(e.target.value)} 
+                                                        required
+                                                    />
                                                 </div>
                                                 <div className="form-group">
                                                     <label>Email de contato</label>
-                                                    <input type="text" className="form-control" placeholder="exemplo@email.com"/>
+                                                    <input 
+                                                        type="email" 
+                                                        defaultValue={market.email} 
+                                                        onChange={e => setEmail(e.target.value)} 
+                                                        className="form-control" 
+                                                        name="email" 
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>CNPJ</label>
+                                                    <input 
+                                                        type="text" 
+                                                        defaultValue={market.cnpj} 
+                                                        onChange={e => setCNPJ(e.target.value)} 
+                                                        className="form-control" 
+                                                        name="cnpj" 
+                                                        required
+                                                    />
                                                 </div>
 
                                                 <button type="submit" className="btn btn-danger">Salvar</button>
                                             </form>
                                         </div>
 
+                                    </div>
+
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <h2>Endereço</h2>
+
+                                        </div>
+
+                                        <div className="card-body">
+                                            <form onSubmit={handleMarketEdit}>
+                                                <div className="form-group">
+                                                    <label>CEP</label>
+                                                    <input 
+                                                        type="number" 
+                                                        defaultValue={market.adress ? market.adress.cep : null} 
+                                                        onChange={e => setCep(e.target.value)} 
+                                                        className="form-control" 
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Endereço</label>
+                                                    <input 
+                                                        type="text" 
+                                                        defaultValue={market.adress ? market.adress.logradouro : null} 
+                                                        onChange={e => setLogradouro(e.target.value)} 
+                                                        className="form-control" 
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Complemento</label>
+                                                    <input 
+                                                        type="text" 
+                                                        defaultValue={market.adress ? market.adress.complemento : null} 
+                                                        onChange={e => setComplemento(e.target.value)} 
+                                                        className="form-control" 
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Bairro</label>
+                                                    <input 
+                                                        type="text" 
+                                                        defaultValue={market.adress ? market.adress.bairro : null} 
+                                                        onChange={e => setBairro(e.target.value)} 
+                                                        className="form-control" 
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>Cidade</label>
+                                                    <input 
+                                                        type="text" 
+                                                        defaultValue={market.adress ? market.adress.localidade : null} 
+                                                        onChange={e => setLocalidade(e.target.value)} 
+                                                        className="form-control" 
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label>UF</label>
+                                                    <select 
+                                                        value={uf ? uf : market.adress ? market.adress.uf : uf}
+                                                        onChange={e => setUf(e.target.value)} 
+                                                        className="form-control" 
+                                                        required
+                                                    >
+                                                        <option value="AC">AC</option>
+                                                        <option value="AL">AL</option>
+                                                        <option value="AP">AP</option>
+                                                        <option value="AM">AM</option>
+                                                        <option value="BA">BA</option>
+                                                        <option value="CE">CE</option>
+                                                        <option value="DF">DF</option>
+                                                        <option value="ES">ES</option>
+                                                        <option value="GO">GO</option>
+                                                        <option value="MA">MA</option>
+                                                        <option value="MT">MT</option>
+                                                        <option value="MS">MS</option>
+                                                        <option value="MG">MG</option>
+                                                        <option value="PA">PA</option>
+                                                        <option value="PB">PB</option>
+                                                        <option value="PR">PR</option>
+                                                        <option value="PE">PE</option>
+                                                        <option value="PI">PI</option>
+                                                        <option value="RJ">RJ</option>
+                                                        <option value="RN">RN</option>
+                                                        <option value="RS">RS</option>
+                                                        <option value="RO">RO</option>
+                                                        <option value="RR">RR</option>
+                                                        <option value="SC">SC</option>
+                                                        <option value="SP">SP</option>
+                                                        <option value="SE">SE</option>
+                                                        <option value="TO">TO</option>
+                                                    </select>
+                                                </div>
+
+                                                <button type="submit" className="btn btn-danger">Salvar</button>
+                                            </form>
+                                        </div>
                                     </div>
 
                                 </div>
